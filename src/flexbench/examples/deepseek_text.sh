f3267421d218dc45be849b78ba71e17081e7f837
Daniel Altunay <daniel.altunay@flex.ai>
2025-03-21 16:09:53 +0100

    add examples

---
+++ src/flexbench/examples/deepseek_text.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Terminal 1: Run the VLLM server
+CUDA_VISIBLE_DEVICES=0 vllm serve \
+    --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B \
+    --max-model-len=2048 \
+    --disable-log-requests \
+    --port 8000
+
+# Terminal 2: Run the FlexBench client
+python main.py \
+    --task text \
+    --model-path deepseek-ai/DeepSeek-R1-Distill-Llama-8B \
+    --api-server http://localhost:8000 \
+    --scenario Server \
+    --target-qps 10 \
+    --dataset-path ctuning/MLPerf-OpenOrca \
+    --dataset-input-column "question" \
+    --dataset-output-column "response" \
+    --dataset-system-prompt-column "system_prompt" \
+    --total-sample-count 200
---
+++ src/flexbench/examples/qwen_vision.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Terminal 1: Run the VLLM server
+CUDA_VISIBLE_DEVICES=0 vllm serve \
+    --model Qwen/Qwen2.5-VL-7B-Instruct \
+    --max-model-len=2048 \
+    --disable-log-requests \
+    --port 8000
+
+# Terminal 2: Run the FlexBench client
+python main.py \
+    --task vision \
+    --model-path Qwen/Qwen2.5-VL-7B-Instruct \
+    --api-server http://localhost:8000 \
+    --scenario Server \
+    --target-qps 10 \
+    --dataset-path philschmid/amazon-product-descriptions-vlm \
+    --dataset-input-column "Product Name" \
+    --dataset-output-column "description" \
+    --dataset-image-column "image" \
+    --total-sample-count 200
