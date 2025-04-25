7de88f7e8b4f472c372f91125b9169599b4a8f2c
Daniel Altunay <daniel.altunay@flex.ai>
2025-04-25 16:59:26 +0200

    Include more metrics (#9)
    Refs: NOTICKET
    
    Some metrics were missing for loadgen server mode, + we don't need to be
    in `src/flexbench` anymore to run a benchmark

--- src/flexbench/__main__.py
+++ src/flexbench/__main__.py
@@ -1,4 +1,9 @@
+import os
+from pathlib import Path
+
 from flexbench.main import main
 
 if __name__ == "__main__":
+    root_dir = Path(__file__).resolve().parent.parent.parent
+    os.chdir(root_dir / "src" / "flexbench")
     main()
